b5a43c71d874696016642ac3a68e3cde1d0b1ca7
cjgmj <cjgmj@hotmail.com>
2020-07-14 19:52:55 +0200

    Stream Flux Observable

--- spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
@@ -1,13 +1,23 @@
 package com.cjgmj.reactor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		final Flux<String> nombres = Flux.just("Joe", "John", "Linus", "Jane").doOnNext(System.out::println);
+
+		nombres.subscribe();
 	}
 
 }
