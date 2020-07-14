7485936fc01f75cf7cc0b9bfec55cd9cad0a5fce
cjgmj <cjgmj@hotmail.com>
2020-07-14 19:59:47 +0200

    Método subscribe

--- spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.cjgmj.reactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +11,23 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		final Flux<String> nombres = Flux.just("Joe", "John", "Linus", "Jane").doOnNext(System.out::println);
+		final Flux<String> nombres = Flux.just("Joe", "John", "", "Linus", "Jane").doOnNext(nombre -> {
+			if (nombre.isEmpty()) {
+				throw new RuntimeException("El nombre no puede estar vacío");
+			}
 
-		nombres.subscribe();
+			System.out.println(nombre);
+		});
+
+		nombres.subscribe(nombre -> LOG.info(nombre), error -> LOG.error(error.getMessage()));
 	}
 
 }
