c930bb49902137c721440eda4020eaf639297165
cjgmj <cjgmj@hotmail.com>
2020-07-17 15:49:20 +0200

    Modificando un Stream reactivo

--- spring-boot-webflux/src/main/java/com/cjgmj/webflux/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/cjgmj/webflux/controllers/ProductoController.java
@@ -1,5 +1,7 @@
 package com.cjgmj.webflux.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +15,19 @@ import reactor.core.publisher.Flux;
 @Controller
 public class ProductoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ProductoController.class);
+
 	@Autowired
 	private ProductoDao productoDao;
 
 	@GetMapping({ "/", "/listar" })
 	public String listar(Model model) {
-		final Flux<Producto> productos = this.productoDao.findAll();
+		final Flux<Producto> productos = this.productoDao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+
+		productos.subscribe(producto -> LOG.info(producto.getNombre()));
 
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
