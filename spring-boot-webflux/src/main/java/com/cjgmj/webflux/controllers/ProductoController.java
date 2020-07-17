6605d5647229f348ed958eba405da537902963bf
cjgmj <cjgmj@hotmail.com>
2020-07-17 15:53:24 +0200

    Modo Reactive Data Driven

--- spring-boot-webflux/src/main/java/com/cjgmj/webflux/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/cjgmj/webflux/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.cjgmj.webflux.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.cjgmj.webflux.models.dao.ProductoDao;
 import com.cjgmj.webflux.models.documents.Producto;
@@ -30,6 +33,21 @@ public class ProductoController {
 		productos.subscribe(producto -> LOG.info(producto.getNombre()));
 
 		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+
+		return "listar";
+	}
+
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		final Flux<Producto> productos = this.productoDao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+
+		productos.subscribe(producto -> LOG.info(producto.getNombre()));
+
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
 		model.addAttribute("titulo", "Listado de productos");
 
 		return "listar";
