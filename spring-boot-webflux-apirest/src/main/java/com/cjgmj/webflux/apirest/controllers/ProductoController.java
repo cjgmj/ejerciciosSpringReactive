ff9e4828d7084b7076c7851719e468016e9fcf00
cjgmj <cjgmj@hotmail.com>
2020-07-26 11:02:19 +0200

    Implementando handler PUT

--- spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,17 @@ public class ProductoController {
 		return this.productoService.save(producto)
 				.map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
 						.contentType(MediaType.APPLICATION_JSON).body(p));
+	}
+
+	@PutMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> editar(@PathVariable String id, @RequestBody Producto producto) {
+		return this.productoService.findById(id).flatMap(p -> {
+			p.setNombre(producto.getNombre());
+			p.setPrecio(producto.getPrecio());
+			p.setCategoria(producto.getCategoria());
+
+			return this.productoService.save(p);
+		}).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 
 }
