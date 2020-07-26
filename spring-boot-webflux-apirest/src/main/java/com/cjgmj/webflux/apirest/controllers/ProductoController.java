831397bd21980dac6e8e9f77d6ff9e238f2bc7a4
cjgmj <cjgmj@hotmail.com>
2020-07-26 11:37:14 +0200

    Implementando handler upload

--- spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
@@ -1,12 +1,16 @@
 package com.cjgmj.webflux.apirest.controllers;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjgmj.webflux.apirest.models.documents.Producto;
@@ -28,6 +33,9 @@ public class ProductoController {
 
 	@Autowired
 	private ProductoService productoService;
+
+	@Value("${config.uploads.path}")
+	private String path;
 
 	@GetMapping("/lista/flux")
 	public Flux<Producto> listaFlux() {
@@ -76,6 +84,16 @@ public class ProductoController {
 				// ResponseEntity.notContent().build() cambiamos Void a Object
 				p -> this.productoService.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT))))
 				.defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+	}
+
+	@PostMapping("/upload/{id}")
+	public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file) {
+		return this.productoService.findById(id).flatMap(p -> {
+			p.setFoto(UUID.randomUUID().toString() + "-"
+					+ file.filename().replace(" ", "").replace(":", "").replace("\\", ""));
+
+			return file.transferTo(new File(this.path + p.getFoto())).then(this.productoService.save(p));
+		}).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 
 }
