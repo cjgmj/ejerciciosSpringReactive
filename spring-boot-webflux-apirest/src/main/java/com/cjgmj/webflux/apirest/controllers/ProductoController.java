3c8d2fbcc38368f6fa632da07ed136b4aac8400e
cjgmj <cjgmj@hotmail.com>
2020-07-26 11:09:49 +0200

    Implementando handler DELETE

--- spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
@@ -4,8 +4,10 @@ import java.net.URI;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +67,15 @@ public class ProductoController {
 
 			return this.productoService.save(p);
 		}).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id) {
+		return this.productoService.findById(id).flatMap(
+				// Para usar ResponseEntity.notFound().build() o
+				// ResponseEntity.notContent().build() cambiamos Void a Object
+				p -> this.productoService.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT))))
+				.defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
 	}
 
 }
