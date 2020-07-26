52b0fc46fe6ee90afb107a464615e239924ee32f
cjgmj <cjgmj@hotmail.com>
2020-07-26 10:54:11 +0200

    Implementando handler POST

--- spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/cjgmj/webflux/apirest/controllers/ProductoController.java
@@ -1,10 +1,15 @@
 package com.cjgmj.webflux.apirest.controllers;
 
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +42,17 @@ public class ProductoController {
 	public Mono<ResponseEntity<Producto>> ver(@PathVariable String id) {
 		return this.productoService.findById(id).map(ResponseEntity::ok)
 				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
+		if (producto.getCreateAt() == null) {
+			producto.setCreateAt(new Date());
+		}
+
+		return this.productoService.save(producto)
+				.map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
+						.contentType(MediaType.APPLICATION_JSON).body(p));
 	}
 
 }
