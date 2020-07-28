8418e4a3d14cf4fbcdcd2e82e439928506cbe513
cjgmj <cjgmj@hotmail.com>
2020-07-28 14:25:01 +0200

    Base aplicación Client

---
+++ spring-boot-webflux-client/src/test/java/com/cjgmj/webflux/client/SpringBootWebfluxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjgmj.webflux.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
