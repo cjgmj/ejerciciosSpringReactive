9bbde002ba282ca85e7ce0bfdeb2b4e7505b9caa
cjgmj <cjgmj@hotmail.com>
2020-07-28 16:48:33 +0200

    Base aplicación eureka

---
+++ spring-boot-webflux-eureka-server/src/test/java/com/cjgmj/webflux/eureka/SpringBootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjgmj.webflux.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
